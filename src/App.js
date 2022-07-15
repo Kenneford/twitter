1cd6e269fb6cee7ff47a181d03664738940011aa
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-07-15 04:09:35 +0200

    Leftpane icons added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import LeftPane from './components/LeftPane';
 import MainPane from './components/MainPane';
--- src/components/LeftPane.js
+++ src/components/LeftPane.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function LeftPane() {
   return (
-    <div className='leftPane'>LeftPane</div>
+    <div className='leftPane'>
+        <div>
+        <i class="fa-brands fa-twitter"></i>
+        </div>
+        <div>
+            <div><i class="fa-solid fa-house"></i> Home</div>
+            <div><i class="fa-regular fa-bell"></i> Notification</div>
+            <div><i class="fa-regular fa-envelope"></i> Message</div>
+            <div><i class="fa-regular fa-user"></i> Profile</div>
+        </div>
+    </div>
   )
 }
