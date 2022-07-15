23e80450f2e1c1f77e01a06ad41f3947fc5b827f
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-07-15 13:00:57 +0200

    Leftpane design completed

--- src/components/LeftPane.js
+++ src/components/LeftPane.js
@@ -1,16 +1,35 @@
 import React from 'react'
+import logo from '../logo.svg'
+import './LeftPane.css'
 
 export default function LeftPane() {
   return (
     <div className='leftPane'>
-        <div>
-        <i class="fa-brands fa-twitter"></i>
-        </div>
-        <div>
-            <div><i class="fa-solid fa-house"></i> Home</div>
-            <div><i class="fa-regular fa-bell"></i> Notification</div>
-            <div><i class="fa-regular fa-envelope"></i> Message</div>
-            <div><i class="fa-regular fa-user"></i> Profile</div>
+        <div className='leftPane-cont'>
+            <div>
+            <i className="fa-brands fa-twitter"></i>
+            </div>
+            <div className='leftPane-icons'>
+                <div><a href=''><i className="fa-solid fa-house"></i> Home</a></div>
+                <div><a href=''><i className="fa-brands fa-wpexplorer"></i> Explore</a></div>
+                <div><a href=''><i className="fa-regular fa-bell"></i> Notification</a></div>
+                <div><a href=''><i className="fa-regular fa-envelope"></i> Message</a></div>
+                <div><a href=''><i className="fa-regular fa-bookmark"></i> Bookmark</a></div>
+                <div><a href=''><i className="fa-brands fa-elementor"></i> Lists</a></div>
+                <div><a href=''><i className="fa-regular fa-user"></i> Profile</a></div>
+                <div><a href=''><i className="fa-brands fa-gg-circle"></i> More</a></div>
+                <button>Tweet</button>
+            </div>
+            <div className='user'>
+                <a href='#'>
+                <img src={logo} alt='user-image' width='50px'/>
+                <div className='user-name'>
+                    <p>JustKenn</p>
+                    <p className='nickName'>@Justkenn</p>
+                </div>
+                <div><i className="fa-brands fa-gg-circle"></i></div>
+                </a>
+            </div>
         </div>
     </div>
   )
